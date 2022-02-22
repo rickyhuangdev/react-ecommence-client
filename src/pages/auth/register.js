42e17faabc5bdf4bf99f2c48d7874f9dc3fe274a
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-02-22 21:41:23 +0800

    create user register with email and password

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,9 +1,26 @@
 import React, {useState} from 'react';
-
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
+import {app}from '../../utils/firebase'
 const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const handleSubmit = (e) => {
+    const handleSubmit =async (e) => {
+        e.preventDefault()
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log(userCredential)
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
 
     }
     const registerForm = () => (
@@ -11,17 +28,15 @@ const Register = () => {
             <form className='mt-5' onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                           onChange={(event => setEmail(event.target.value))}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <input type="password" className="form-control" id="exampleInputPassword1"
+                           onChange={(event => setPassword(event.target.value))}/>
                 </div>
-                <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-success">Submit</button>
             </form>
 
     )
@@ -29,6 +44,7 @@ const Register = () => {
     return (
         <div className="container py-5">
             <div className="row d-flex align-items-center" style={{minHeight:'65vh'}}>
+                <ToastContainer/>
                 <div className="col col-md-6 offset-md-3 mx-auto">
                     <h4>Register</h4>
                     {registerForm()}
