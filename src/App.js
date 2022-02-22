aa86e5e79438d9827479452669bc2c581cc848c2
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-02-22 22:17:23 +0800

    create user register notification and set user token

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'antd/dist/antd.css'
 import './assets/css/bootstrap.min.css';
 import {Route, Switch} from "react-router-dom";
 import React, {Fragment} from 'react';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import Home from "./pages/home";
@@ -12,6 +14,7 @@ function App(props) {
   return (
       <Fragment>
           <Header/>
+          <ToastContainer />
           <Switch>
               <Route path='/' component={Home} exact/>
               <Route path='/login' component={Login}/>
--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,24 +1,69 @@
 import React, {useState} from 'react';
-import {ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import {toast} from 'react-toastify';
+
 import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
 import {app}from '../../utils/firebase'
+import {setTokenInfo} from "../../utils/storage";
 const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const handleSubmit =async (e) => {
         e.preventDefault()
+        if (!email || email ===''){
+            toast.warning("Please fill in the email")
+            return
+        }
+        if (!password || password===''){
+            toast.warning("Please fill in the password")
+            return
+        }
         const auth = getAuth();
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in
                 const user = userCredential.user;
+                setTokenInfo(userCredential._tokenResponse.refreshToken)
+                toast.success('🦄 Register Successfully!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
                 console.log(userCredential)
                 // ...
             })
+
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
+                return
+                let message = null
+                switch (errorCode){
+                    case 'auth/weak-password':
+                        message ="Password should be at least 6 characters"
+                        break;
+                        case 'auth/email-already-in-use':
+                        message ="This email address is already being used"
+                        break;
+                    default:
+                        message = null;
+                }
+
+                    toast.error(`${message}`, {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
+
                 // ..
             });
 
@@ -44,7 +89,6 @@ const Register = () => {
     return (
         <div className="container py-5">
             <div className="row d-flex align-items-center" style={{minHeight:'65vh'}}>
-                <ToastContainer/>
                 <div className="col col-md-6 offset-md-3 mx-auto">
                     <h4>Register</h4>
                     {registerForm()}
