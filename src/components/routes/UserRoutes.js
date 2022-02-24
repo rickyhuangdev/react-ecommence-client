47647aad76ca27182bb1f5dbe3bd75abea150790
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-02-24 14:26:51 +0800

    redirect user if not login

--- src/components/routes/UserRoutes.js
+++ src/components/routes/UserRoutes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {useSelector} from "react-redux";
-import {Route} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
 
 const UserRoutes = ({children, ...rest}) => {
     const user = useSelector(state => state.profile.user)
     return user && user.user_id ? (
-        <Route {...rest} render={() => children}/>) : (<h1>Loading</h1>)
+        <Route {...rest} render={() => children}/>) : (<Redirect to="/login"></Redirect>)
 };
 
 export default UserRoutes;
