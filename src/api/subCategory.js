36aab4dde076d516c5665853570011a525417211
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-02-25 15:00:56 +0800

    create subCategory api request

---
+++ src/api/subCategory.js
@@ -0,0 +1,17 @@
+import request from "../utils/request";
+
+export const getSubCategoryApi = () => {
+    return request('/subCategory', 'get')
+}
+export const readSubCategoryApi = (slug) => {
+    return request(`/subCategory/${slug}`, 'get')
+}
+export const updateSubCategoryApi = (data) => {
+    return request(`/SubCategory/${data.slug}`, 'put', data)
+}
+export const deleteSubCategoryApi = (slug) => {
+    return request(`/subCategory/${slug}`, 'delete')
+}
+export const createSubCategoryApi = data => {
+    return request('/subCategory', 'post', data)
+}
