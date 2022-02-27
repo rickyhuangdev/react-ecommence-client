c27571469f1bbc51fd1d5b0e054922ced7e9fb3f
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-02-27 19:10:51 +0800

    FileResizer in react

--- src/components/form/FileUpload.js
+++ src/components/form/FileUpload.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import FileResizer from "react-image-file-resizer";
 
 const FileUpload = () => {
-    const fileUploadAndResize = (e)=>{
-        console.log(e.target.files)
+    const fileUploadAndResize = (e)=> {
+        let files = e.target.files
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                FileResizer.imageFileResizer(files[i], 720, 720, 'JPEG', 100, 0, (uri) => {
+                        console.log(uri);
+                    },
+                    "base64")
+            }
+        }
     }
     return (
         <div className="form-group">
