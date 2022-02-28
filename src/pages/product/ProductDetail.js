3893f4ec729b0c7f60e81f233e90f55d76cdfb5d
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-01 00:52:09 +0800

    create product image slider

--- src/pages/product/ProductDetail.js
+++ src/pages/product/ProductDetail.js
@@ -1,22 +1,74 @@
-import React from 'react';
-import {BsBookmarks, BsHeart, BsRecordFill, BsShare, BsStar} from "react-icons/bs";
+import React, {useEffect, useState} from 'react';
+import {BsBookmarks, BsHeart, BsShare, BsStar} from "react-icons/bs";
 import {InputNumber, Space} from 'antd';
-
-const ProductDetail = () => {
+import {readProductApi} from "../../api/product";
+import Slider from "react-slick";
+import '../../assets/css/product.css'
+const ProductDetail = ({match}) => {
+    const {slug} = match.params
+    const [product, setProduct] = useState({});
+    const [imageSlider, setImageSlider] = useState([]);
+    useEffect(() => {
+        getProductDetail()
+    }, [])
+    const getProductDetail = () => {
+        readProductApi(slug).then(res => {
+            if (res) {
+                setProduct(res)
+                setImageSlider(res.images)
+            }
+        })
+    }
     const onChange = () => {
     }
+    const thumbImageStyle ={
+
+    }
+    const settings = {
+        customPaging: function (i) {
+            return (
+                <a>
+                    <img src={imageSlider[i].url} />
+                </a>
+
+
+            );
+        },
+        dots: true,
+        dotsClass: "slick-dots slick-product-thumb",
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide:2
+    };
 
     return (
         <div className="product-details">
-            <div className="container p-5">
+            <div className="container-fluid p-4 p-sm-5 ">
                 <div className="row">
-                    <div className="col-xl-6">
+                    <div className="col-xl-5">
+                        <div className="product__details-nav">
+                            <div className="product__details-thumb">
+                                <Slider {...settings}>
+                                    {imageSlider && imageSlider.map(item => (
+                                        <div key={item.public_id} className="d-flex justify-content-center align-items-start">
+                                            <img src={item.url} alt={item.public_id} style={{height:"480px"}} className="product-slider-image" />
+                                        </div>
+                                    ))}
 
+                                </Slider>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-xl-6">
+                    <div className="col-xl-7 product__details-content-description">
                         <div className="product__details-content"><h6
-                            style={{color: '#0068c9', fontSize: '24px', margin: '0 0 8px', fontWeight: "700"}}>Samsung
-                            Galaxy A12,32GB,Black –(Locked)</h6>
+                            style={{
+                                color: '#0068c9',
+                                fontSize: '24px',
+                                margin: '0 0 8px',
+                                fontWeight: "700"
+                            }}>{product.title}</h6>
                             <div className="pd-rating mb-10">
                                 <ul className="rating d-flex" style={{listStyle: 'none'}}>
                                     <li><a href="#"><BsStar/></a></li>
@@ -26,25 +78,19 @@ const ProductDetail = () => {
                                     <li><a href="#"><BsStar/></a></li>
                                 </ul>
                                 <span>(01 review)</span><span><a href="#">Add your review</a></span></div>
-                            <div className="price mb-10"><span>$216.00</span></div>
+                            <div className="price mb-10"><span>$ {product.price}</span></div>
                             <div className="features-des mb-20 mt-10">
                                 <ul>
-                                    <li><a href="product-details.html"><BsRecordFill/>Bass and Stereo
-                                        Sound.</a></li>
-                                    <li><a href="product-details.html"><BsRecordFill/>Display with
-                                        3088 x 1440 pixels resolution.</a></li>
-                                    <li><a href="product-details.html"><BsRecordFill/>Memory,Storage &amp;SIM:12GB
-                                        RAM,256GB.</a></li>
-                                    <li><a href="product-details.html"><BsRecordFill/>Androi v10.0
-                                        Operating system.</a></li>
+                                    <li><span className="text-dark font-weight-bold">{product.description}</span></li>
                                 </ul>
                             </div>
-                            <div className="product-stock mb-20"><h5>Availability:<span>940 in stock</span></h5></div>
+                            <div className="product-stock mb-20">
+                                <h5>Availability:<span>{product.quantity} in stock</span></h5></div>
                             <div className="cart-option mb-15">
                                 <div className="product-quantity mr-20">
                                     <div className="cart-plus-minus p-relative">
                                         <Space>
-                                            <InputNumber size="large" min={1} max={100000} defaultValue={3}
+                                            <InputNumber size="large" min={1} max={product.quantity} defaultValue={1}
                                                          onChange={onChange}/>
                                         </Space>
                                     </div>
@@ -55,13 +101,23 @@ const ProductDetail = () => {
                             </div>
                             <div className="details-meta">
                                 <div className="d-meta-left">
-                                    <div className="dm-item mr-20"><a href="#" ><BsHeart className="mr-1"/>Add to
-                                        wishlist</a></div>
-                                    <div className="dm-item"><a href="#"><BsBookmarks className="mr-1"/>Compare</a>
+                                    <div className="dm-item mr-20">
+                                        <button className="btn btn-secondary shadow-none d-flex align-items-center">
+                                            <BsHeart className="mr-1 text-white font-weight-bold"/>Add to
+                                            wishlist
+                                        </button>
+                                    </div>
+                                    <div className="dm-item">
+                                        <button className="btn btn-light shadow-none d-flex align-items-center">
+                                            <BsBookmarks className="mr-1"/>Compare
+                                        </button>
                                     </div>
                                 </div>
                                 <div className="d-meta-left">
-                                    <div className="dm-item"><a href="#"><BsShare className="mr-1"/>Share</a>
+                                    <div className="dm-item">
+                                        <button className="btn btn-info shadow-none d-flex align-items-center"><BsShare
+                                            className="mr-1"/>Share
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -69,7 +125,9 @@ const ProductDetail = () => {
                                 <div className="product_info"><span className="sku_wrapper"><span
                                     className="title">SKU:</span><span className="sku">DK1002</span></span><span
                                     className="posted_in"><span className="title">Categories:</span><a
-                                    href="#">iPhone</a><a href="#">Tablets</a></span><span className="tagged_as"><span
+                                    href="#">
+                                    {/*{product.category.name}*/}
+                                    </a></span><span className="tagged_as"><span
                                     className="title">Tags:</span><a href="#">Smartphone</a>,<a
                                     href="#">Tablets</a></span></div>
                             </div>
