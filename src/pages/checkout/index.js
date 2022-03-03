f5bcce770121ebc62cc3729fbee36c54f3ec2c6b
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-03 17:37:37 +0800

    create Order Summary form

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -123,6 +123,52 @@ const CheckOutIndex = () => {
                         </div>
                     </div>
                     <div className="col-lg-6">
+                        <div className="your-order mb-30 ">
+                            <h3>Order Summary</h3>
+                            <div className="your-order-table table-responsive">
+                                <table>
+                                    <thead>
+                                    <tr>
+                                        <th className="product-name">Product</th>
+                                        <th className="product-total">Total</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr className="cart_item">
+                                        <td className="product-name">Vestibulum suscipit <strong
+                                            className="product-quantity">× 1</strong></td>
+                                        <td className="product-total"><span className="amount">$165.00</span></td>
+                                    </tr>
+                                    <tr className="cart_item">
+                                        <td className="product-name">Vestibulum dictum magna <strong
+                                            className="product-quantity">× 1</strong></td>
+                                        <td className="product-total"><span className="amount">$50.00</span></td>
+                                    </tr>
+                                    </tbody>
+                                    <tfoot>
+                                    <tr className="cart-subtotal">
+                                        <th>Cart Subtotal</th>
+                                        <td><span className="amount">$215.00</span></td>
+                                    </tr>
+                                    <tr className="shipping">
+                                        <th>Shipping</th>
+                                        <td>
+                                            <ul>
+                                                <li><input type="radio" name="shipping"/><label>Flat Rate:<span
+                                                    className="amount">$7.00</span></label></li>
+                                                <li><input type="radio" name="shipping"/><label>Free Shipping:</label>
+                                                </li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr className="order-total">
+                                        <th>Order Total</th>
+                                        <td><strong><span className="amount">$215.00</span></strong></td>
+                                    </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
