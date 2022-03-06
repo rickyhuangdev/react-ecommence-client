e20867588e9c0ffa10bb0a98dedd41e685a90b7e
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-06 10:58:11 +0800

    update the user page layout

--- src/pages/user/dashboard.js
+++ src/pages/user/dashboard.js
@@ -8,10 +8,10 @@ const Dashboard = () => {
     return (
         <div className="container py-5">
             <div className="row align-items-start justify-content-between">
-                <div className="col-12 col-md-12 col-lg-4 col-xl-4 miliods">
+                <div className="col-12 col-md-4">
                     <UserNav/>
                 </div>
-                <div className="col-12 col-md-12 col-lg-8 col-xl-8">
+                <div className="col-12 col-md-8 mt-5 mt-md-0">
                     <div className="container py-5">
                         <div className="row">
                             <div className="col">
